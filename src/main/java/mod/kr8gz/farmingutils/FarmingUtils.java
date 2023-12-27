a900545d3bd299111b45c312540867809674c5b4
kr8gz <47776344+kr8gz@users.noreply.github.com>
2023-12-27 13:54:49 +0100

    i decided on 1.0.1 after all, ready for release!

--- src/main/java/mod/kr8gz/farmingutils/FarmingUtils.java
+++ src/main/java/mod/kr8gz/farmingutils/FarmingUtils.java
@@ -20,7 +20,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class FarmingUtils {
     public static final String MODID = "farmingutils";
     public static final String NAME = "FarmingUtils";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.0.1";
 
     public FarmingUtils() {
         Helper.createDirectory(FarmingUtils.MODID + "/config");
@@ -36,5 +36,4 @@ public class FarmingUtils {
     public static void preInit(FMLPreInitializationEvent event) {
         ClientCommandHandler.instance.registerCommand(new CommandModConfig());
     }
-
 }
